5d67698f6d07e369f4aeb406f1130252344f100a
vfcastro <vinicius_decastro@vinicius>
2018-05-10 12:20:12 -0300

    add mlreport

--- invaders.r
+++ invaders.r
@@ -3,18 +3,19 @@ if (interactive()) {
 }
 
 # Cria o jogador
-MakePlayer <- function(x=0, y=0, xsize=3, ysize=1, life=3, ammo=3){
-	return (list(x = x, y = y, xsize=xsize, ysize=ysize, life = life, ammo = ammo))
+MakePlayer <- function(x=15, y=0.5, xsize=3, ysize=1, life=3, ammo=3, shots=list()){
+	return (list(x = x, y = y, xsize=xsize, ysize=ysize, life = life, ammo = ammo, shots=shots))
 }
 
 # Atualiza o player dependendo do botão pressionado
 UpdatePlayer <- function(key, player){
 	if(key == "a")
-	  return (MakePlayer(x=player[["x"]]-1, y=player[["y"]]))
+	  return (MakePlayer(x=player[["x"]]-1, y=player[["y"]], shots=player[["shots"]]))
 	if(key == "d")
-	  return (MakePlayer(x=player[["x"]]+1, y=player[["y"]]))
+	  return (MakePlayer(x=player[["x"]]+1, y=player[["y"]], shots=player[["shots"]]))
 	if(key == "w")
-	  return (MakePlayer(x=player[["x"]]-1, y=player[["y"]], ammo=player[["ammo"]]-1))
+	  return (MakePlayer(x=player[["x"]], y=player[["y"]], 
+											 shots=list(append(player[["shots"]],MakeShot(player[["x"]],player[["y"]]+1)))))
 	return(player)
 }
 
@@ -22,7 +23,6 @@ UpdatePlayer <- function(key, player){
 DrawPlayer <- function(player){
 	symbols(player[["x"]], player[["y"]], rectangles = matrix(c(player[["xsize"]],player[["ysize"]]), ncol = 2),
 	          inches = FALSE, fg = "white", bg = "white", add = TRUE	)
-	print(player)
 }
 
 # Apaga o jogador (desenha em preto)
@@ -44,18 +44,41 @@ TestScreenSpace <- function(player,height,width){
 }
 
 
+# Cria um tiro (vamos ter 2 lista, uma pros tiros do player e outra pras naves)
+MakeShot <- function(x, y, xsize=1, ysize=1){
+  return (list(x = x, y = y, xsize=xsize, ysize=ysize))
+}
+
+# Desenha o tiro
+DrawShot <- function(shot){
+	symbols(shot[["x"]], shot[["y"]], rectangles = matrix(c(shot[["xsize"]],shot[["ysize"]]), ncol = 2),
+	          inches = FALSE, fg = "white", bg = "white", add = TRUE	)
+}
+
+# Desenha o tiro
+EraseShot <- function(shot){
+	symbols(shot[["x"]], shot[["y"]], rectangles = matrix(c(shot[["xsize"]],shot[["ysize"]]), ncol = 2),
+	          inches = FALSE, fg = "black", bg = "black", add = TRUE	)
+}
+
+# Atualiza tiro do jogador
+UpdateShot <- function(shot){
+	return(MakeShot(x=shot[["x"]], y=shot[["y"]]+1))
+}
+
+
 game <- function(height=30,width=30){
 	# Cria a janela do jogo
 	par(mar = rep(1,4), bg = "black")
 	plot.new()
 	plot.window(xlim = c(0, width), ylim = c(0, height))
-
 	# Desenha as bordas
 	DrawBorders(height,width)
 	
-	# Cria o jogador na posicao inicial
+	# Cria o jogador
 	player <- MakePlayer(x=15,y=0.5)
-	
+	playerShots <- list()
+		
   # Desenha o jogador
 	DrawPlayer(player)
 
@@ -65,6 +88,7 @@ game <- function(height=30,width=30){
 			graphics.off()
 		else
 			return(UpdatePlayer(key,player))
+			
 	}
 
 	# Loop principal
@@ -78,6 +102,20 @@ game <- function(height=30,width=30){
 			DrawBorders(height,width)
 			DrawPlayer(player)
 		}		
+
+		# Renderiza tiros do jogador
+		lapply(player[["shots"]],DrawShot)
+
+		# Apaga tiros do jogador
+		lapply(player[["shots"]],EraseShot)
+
+  	# Atuliza tiros do jogador
+		newshots <- lapply(player[["shots"]],UpdateShot)
+		player <- MakePlayer(x=player[["x"]], y=player[["y"]], shots=newshots)
+
+		# Renderiza tiros do jogador
+		lapply(player[["shots"]],DrawShot)
+	
 
 
 		Sys.sleep(.01)
